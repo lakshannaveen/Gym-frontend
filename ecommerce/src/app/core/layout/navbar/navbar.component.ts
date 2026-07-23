288354d4033545e26992d2724c31e26a184d4b84
Naveen De Silva <139128804+lakshannaveen@users.noreply.github.com>
2026-07-23 10:48:25 +0530

    done

--- ecommerce/src/app/core/layout/navbar/navbar.component.ts
+++ ecommerce/src/app/core/layout/navbar/navbar.component.ts
@@ -1,19 +1,29 @@
 // navbar.component.ts
 import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   isScrolled = false;
+  isMobileMenuOpen = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 20;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
   }
 }
