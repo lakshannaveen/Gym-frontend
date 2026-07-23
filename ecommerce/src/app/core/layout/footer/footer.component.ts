19dd6b76130483a6c2586232e3d64f2f6efed945
Naveen De Silva <139128804+lakshannaveen@users.noreply.github.com>
2026-07-23 11:40:48 +0530

    done

--- ecommerce/src/app/core/layout/footer/footer.component.ts
+++ ecommerce/src/app/core/layout/footer/footer.component.ts
@@ -1,12 +1,25 @@
+// footer.component.ts
 import { Component } from '@angular/core';
-import { BACKEND_URL } from '../../../shared/config/backend-url';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  protected readonly backendUrl = BACKEND_URL;
+  // Social media links - replace with your actual URLs
+  socialLinks = {
+    facebook: 'https://facebook.com/skysatlanka',
+    instagram: 'https://instagram.com/skysatlanka',
+    youtube: 'https://youtube.com/skysatlanka',
+    whatsapp: 'https://wa.me/94774235919',
+    twitter: 'https://twitter.com/skysatlanka'
+  };
+currentYear: any;
+
+  // You can also add backend URL if needed
+  // backendUrl = 'https://api.skysatlanka.com';
 }
